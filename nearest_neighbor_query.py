75dcbb498186d3096f5d30c10ee19e682b78f329
Vanessa Meschke <vmeschke@wisc.edu>
2017-11-06 10:59:14 -0600

    Updating to query OQMD
    added some import statements

--- nearest_neighbor_query.py
+++ nearest_neighbor_query.py
@@ -1,1 +1,6 @@
 # Use SKLearn's Nearest Neighbors function to find the closest values from the OQMD data base
+from sklearn.neighbors import nearestneighbors
+import pandas
+
+# Read in the formulas we want to find
+miracle_formulas = pandas.read_csv("C:\Users\mvane\Documents\Skunkworks\BMG\Miracle_Binary_Formulas.csv")
