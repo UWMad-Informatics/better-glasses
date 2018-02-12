7c4a78a0d0081b816e92edd41241d379ff86e2fc
Vanessa Meschke <vmeschke@wisc.edu>
2018-02-12 11:55:40 -0600

    Formula.csv upload
    accidentally uploaded a .xlsx instead of a .csv for the formulas.

--- forge_query.py
+++ forge_query.py
@@ -18,7 +18,7 @@ mdf.match_elements(["Ca", "Mg", "Zn"])
 # Search for entries with a specific element. Here, search for the largest element perhaps?
 # Advanced args (match field or source) cleared when we call search(), so add reset_query = false to keep matches
 # Use mdf.aggregate() if we need to retrieve > 10000 results from OQMD
-results = mdf.search(limit=10, reset_query=False)
+results = mdf.search(reset_query=False)
 
 # This doesn't work for me yet
 # Download results. You need to have your computer set up to be an endpoint with globus and have it running 
