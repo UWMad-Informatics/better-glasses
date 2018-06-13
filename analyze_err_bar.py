9d5e56c841c504289f747e9ca271b8aa2b72e63b
Vanessa Meschke <vmeschke@wisc.edu>
2018-06-13 15:45:11 -0500

    made plotting
    Plotting error bars & gaussian distribution

--- analyze_err_bar.py
+++ analyze_err_bar.py
@@ -7,6 +7,8 @@ import pandas as pd
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import math
 from citrination_client.client import CitrinationClient
 from citrination_client.util.quote_finder import quote
 
@@ -34,7 +36,7 @@ def main():
 	# predictions = client.models.predict(model_num, input)
 	
 	# # Make a CSV to write predictions to. We'll save formula, Tg, Tl, Tx, and their uncertainties (loss)
-	# # CSV newline = '' prevents CSV from saving with lines between each row.
+	# # CSV arg newline = '' prevents CSV from saving with lines between each row.
 	outfile = "C:/Users/mvane/Documents/Skunkworks/BMG/Data/retrieved_predictions.csv"
 	# with open(outfile, 'w', newline='') as csvfile:
 		# writer = csv.writer(csvfile)
@@ -58,12 +60,15 @@ def main():
 	# Analyze the outputs
 	# Read in the predicted values from the CSV just created
 	predicted_vals = pd.read_csv(outfile)
-	tg_pred = predicted_vals['Tg (K)'].tolist()
-	tl_pred = predicted_vals['Tl (K)'].tolist()
-	tx_pred = predicted_vals['Tx (K)'].tolist()
+	tg_pred = predicted_vals['Tg (K)']
+	tl_pred = predicted_vals['Tl (K)']
+	tx_pred = predicted_vals['Tx (K)']
 	tg_act = exp_data['PROPERTY: Tg (K)'].tolist()
 	tl_act = exp_data['PROPERTY: Tl (K)'].tolist()
 	tx_act = exp_data['PROPERTY: Tx (K)'].tolist()
+	trg_act = exp_data['PROPERTY: Trg']
+	gamma_act = exp_data['PROPERTY: $\\gamma$']
+	omega_act = exp_data['PROPERTY: $\\omega$']
 	tg_loss = predicted_vals['Tg Err'].tolist()
 	tl_loss = predicted_vals['Tl Err'].tolist()
 	tx_loss = predicted_vals['Tx Err'].tolist()
@@ -74,6 +79,11 @@ def main():
 	tg_err_bar = []
 	tl_err_bar = []
 	tx_err_bar = []
+	
+	# GFA Metrics
+	trg_calc = tg_pred/tl_pred
+	gamma_calc = tx_pred/(tg_pred + tl_pred)
+	omega_calc = (tg_pred/tx_pred) - 2*(tg_pred/(tg_pred + tl_pred))
 	
 	for i in range(0, len(formula)):
 		tg_err = abs(tg_pred[i] - tg_act[i])
@@ -87,37 +97,36 @@ def main():
 		tx_err = abs(tx_pred[i] - tx_act[i])
 		tx_act_err.append(tx_err)
 		tx_err_bar.append(abs(tx_err - tx_loss[i]))
-		
-	# Calculate standard deviations to compare output to
-	tg_1std = [np.mean(tg_act) - np.std(tg_act), np.mean(tg_act) - np.std(tg_act)]
-	tg_2std = [np.mean(tg_act) - 2*np.std(tg_act), np.mean(tg_act) + 2*np.std(tg_act)]
-	tg_3std = [np.mean(tg_act) - 3*np.std(tg_act), np.mean(tg_act) + 3*np.std(tg_act)]
 	
-	tl_1std = [np.mean(tl_act) - np.std(tl_act), np.mean(tl_act) - np.std(tl_act)]
-	tl_2std = [np.mean(tl_act) - 2*np.std(tl_act), np.mean(tl_act) + 2*np.std(tl_act)]
-	tl_3std = [np.mean(tl_act) - 3*np.std(tl_act), np.mean(tl_act) + 3*np.std(tl_act)]
-	
-	tx_1std = [np.mean(tx_act) - np.std(tx_act), np.mean(tx_act) - np.std(tx_act)]
-	tx_2std = [np.mean(tx_act) - 2*np.std(tx_act), np.mean(tx_act) + 2*np.std(tx_act)]
-	tx_3std = [np.mean(tx_act) - 3*np.std(tx_act), np.mean(tx_act) + 3*np.std(tx_act)]
-		
-	# Plot settings (make font larger)
-	plt.rc('font', size=20)          
-	plt.rc('axes', titlesize=20)     
-	plt.rc('axes', labelsize=20)
-	plt.rc('xtick', labelsize=20)
-	plt.rc('ytick', labelsize=20)
-	plt.rc('figure', titlesize=20)
-	
-	plt.figure()
-	plt.scatter(tg_act_err, tg_err_bar)
-	plt.title("Trg Error Bar Analysis")
-	plt.xlabel("Actual Tg Error")
-	plt.ylabel("Tg Error Bar")
+	# Plotting Gaussian vs. standard deviations	
+	mu = np.mean(tg_loss)
+	sigma = np.std(tg_loss)
+	n_bins = 100
+	x = tg_loss
+	ra = [min(tg_loss), max(tg_loss)]
+
+	fig, ax = plt.subplots(figsize=(8, 4))
+
+	# plot the cumulative histogram
+	n, bins, patches = ax.hist(tg_loss, n_bins, [min(tg_loss), max(tg_loss)], normed=1, histtype='step', 
+								cumulative=True, label='Tg Error', linewidth=3)
+
+	# Add a line showing the expected distribution.
+	y = mlab.normpdf(bins, mu, sigma).cumsum()
+	y /= y[-1]
+	ax.plot(bins, y, 'k--', linewidth=3, label='Gaussian')
+
+	# tidy up the figure
+	ax.grid(True)
+	ax.legend(loc='right',fontsize=20)
+	ax.set_title('Cumulative step histograms', fontsize=24)
+	ax.set_xlabel('Tg Error (K)', fontsize=22)
+	ax.set_ylabel('Likelihood of occurrence',fontsize=22)
+	plt.tick_params(axis='both', labelsize=16)
 	plt.tight_layout()
 	plt.show()
 
-		
+	
 # Run the script:
 if __name__ == '__main__':
-    main()
+	main()
