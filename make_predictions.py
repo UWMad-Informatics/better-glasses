236fdb063ec8be1453748de00d2974e33fdfff08
Vanessa Meschke <vmeschke@wisc.edu>
2018-06-14 09:05:53 -0500

    Update prediction method for new version of client

--- make_predictions.py
+++ make_predictions.py
@@ -1,6 +1,7 @@
 import os
 import sys
-import json
+import datetime
+import csv
 import requests
 import datetime
 import pandas as pd
@@ -18,18 +19,46 @@ def main():
 	energy = exp_data['PROPERTY: Nearest DFT Formation Energy (eV)'].as_matrix()
 	form = "formula"
 	property = "Property Nearest DFT Formation Energy"
-	# Convert formulas to dictionary one at a time and make predictions. Export each to their own json.	
+	# Convert formulas & energies to dictionary. Make a list of dictionaries.
+	input = []
 	for i in range(0, len(formula)):
-		input = [{form: formula[i], property: energy[i]}]
-		# Make predictions of Tg, Tx, and Tl. (These will also contain many of the Magpie descriptors used to train the model)
-		model_num = "4416"
-		predictions = client.predict(model_num, input)
+		input.append({form: formula[i], property: energy[i]})
 	
-		# Write all these predictions to json files
-		# Folder path:
-		folder_out = "C:/Users/mvane/Documents/GitHub/better-glasses/predictions_output/"
-		with open(folder_out + str(formula[i]) + ".txt", 'w') as outfile:
-			json.dump(predictions, outfile)
+	# Make predictions of Tg, Tx, and Tl. (These will also contain many of the Magpie descriptors used to train the model)
+	model_num = "4416"
+	predictions = client.predict(model_num, input)
+	
+	# Write all these predictions to json files with date and time to differentiate predictions.
+	# Folder path:
+	folder_out = "C:/Users/mvane/Documents/GitHub/better-glasses/predictions_output.csv"
+	first = True
+	# Make CSV writer
+	with open(folder_out, 'w', newline='') as csvfile:
+		writer = csv.writer(csvfile)
+		# Go through every prediction that we made from the chemical formula
+		for p in predictions:
+			keys = p.all_keys()
+			# Make a header row with all of the keys listed
+			if first:
+				row = []
+				row.append('Formula')
+				for k in keys:
+					row.append(k)
+					row.append('Uncertainty')
+				writer.writerow(row)
+				first = False
+			row = []
+			# Write the formula in the first column of the CSV. Formula will also show up in a later column.
+			row.append(p.get_value('formula').value)
+			# Write the value and uncertainty of each key to the CSV
+			for k in keys:
+				val = p.get_value(k).value
+				loss = p.get_value(k).loss
+				row.append(str(val))
+				row.append(str(loss))
+			writer.writerow(row)
+
+	csvfile.close()
 		
 # Run the script:
 if __name__ == '__main__':
