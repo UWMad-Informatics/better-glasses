bda6ae99e3ed1e7ada863c8e4f6cf106dcbc28a3
Vanessa Meschke <vmeschke@wisc.edu>
2018-06-18 09:30:21 -0500

    Cleaning up file
    Cleaning up script to make it more readable

--- update_dataset.py
+++ update_dataset.py
@@ -10,49 +10,33 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 def main():
-	# Read in new data. For test we are using just chemical formula, DFT energy, and only predicting Tg
-	new_data = pd.read_csv("test_10.csv")
-	form = new_data['formula']
-	energy = new_data['PROPERTY: Nearest DFT Formation Energy (eV)']
-	tg = new_data['PROPERTY: Tg (K)']
-
-	# Convert CSV to use with PYPIF to go csv -> PIF
-	input = []
-	# Make pifs
-	for i in range(0, len(form)):
-		chemical_system = ChemicalSystem()
-		chemical_system.chemical_formula = form[i]
-		dft_energy = Property(name = 'Nearest DFT Formation Energy', units = 'eV', scalars = float(energy[i]))
-		tg_prop = Property(name = 'Tg', units = 'K', scalars = float(tg[i]))
-		chemical_system.properties = [dft_energy, tg_prop]
-
-		input.append(chemical_system)
-		
-	# Dictionary to PIF
-	# Write the string that was dumped to a json
-	# with open('pifs.txt', 'w') as outfile:
-		# json.dump(input, outfile)
-	filename = "pif.json"
-	with open(filename, 'w') as fp:
-		pif.dump(input, fp)
-
-	# Upload new data
-	# Do I need to upload, update, create new version...? Does uploading a csv work?
 	# Create instance of the client
 	client = CitrinationClient(environ["CITRINATION_API_KEY"], 'https://citrination.com')
-
-	# # Upload data. Params are (dataset id, file path (local))
+	# dataset_id is the id of the dataset you want to upload new data to
 	dataset_id = 161880
-	# dataview_id = 4743
-	client.data.upload(dataset_id, 'pif.json')
+	# dataview_id is the DataView you want to update after file upload 
+	dataview_id = 4743
 	
-	# Click the save button in the ML config page
-	url = 'https://citrination.com/data_views/4743/ml_config'
-	click_save(url)
+	# Read in new data. For test we are using just chemical formula, DFT energy, and only predicting Tg
+	new_files = ["test_10.csv"]
+	for file in new_files:
+		pif_output = make_pif(file)
 
+		# Upload data. Params are (dataset id, file path (local))
+		client.data.upload(dataset_id, 'pif.json')
 		
-# Save button clicker
+		# Click the save button in the ML config page
+		url = 'https://citrination.com/data_views/' + str(dataview_id) + '/ml_config'
+		click_save(url)
+
+
 def click_save(my_url):
+	"""
+	Method to click the save button in Data Views. Set up to login in using a UW email.
+	
+	:param my_url: URL of the DataView you'd like to click the save button for
+	:type my_url: string
+	"""
 	driver = webdriver.Chrome('C:\Program Files\chromedriver')
 	driver.get(my_url)
 	# Select access type. This is open access xpath.
@@ -72,6 +56,46 @@ def click_save(my_url):
 	# Click the save button to trigger model retrain in data views
 	driver.implicitly_wait(10)
 	button = driver.find_element_by_xpath("/html/body/div/div[3]/div[2]/div[1]/div/div[3]/div/button").click()
+	
+def make_pif(filename):
+	"""
+	Method to turn a CSV file to PIF for this BMG-based dataset. Can edit it to pull out whatever properties needed.
+	
+	:param: filename: Path to the CSV file you'd like to convert to PIF.
+	:type filename: str
+	:return: Path to the JSON output from converting to PIF
+	:rtype: str
+	"""
+	
+	# Read in csv file using pandas to make df
+	new_data = pd.read_csv(filename)
+	# Pull out the desired properties and turn them to lists.
+	form = new_data['formula']
+	energy = new_data['PROPERTY: Nearest DFT Formation Energy (eV)']
+	tg = new_data['PROPERTY: Tg (K)']
+
+	# Convert CSV to use with PYPIF to go csv -> PIF
+	input = []
+	# Make pifs
+	for i in range(0, len(form)):
+		chemical_system = ChemicalSystem()
+		chemical_system.chemical_formula = form[i]
+		dft_energy = Property(name = 'Nearest DFT Formation Energy', units = 'eV', scalars = float(energy[i]))
+		tg_prop = Property(name = 'Tg', units = 'K', scalars = float(tg[i]))
+		chemical_system.properties = [dft_energy, tg_prop]
+
+		input.append(chemical_system)
+	
+	# Dictionary to PIF
+	# Write the string that was dumped to a json
+	# with open('pifs.txt', 'w') as outfile:
+		# json.dump(input, outfile)
+	outfile = filename + "pif.json"
+	with open(outfile, 'w') as fp:
+		pif.dump(input, fp)
+		
+	return outfile
+
 	
 # Run the script:
 if __name__ == '__main__':
